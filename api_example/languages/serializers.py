1f4550bd3610470047db24609622d845d8833764
mahidulmoon <mahidulmoon@gmail.com>
2020-09-16 20:24:17 +0600

    ep6 complete

--- api_example/languages/serializers.py
+++ api_example/languages/serializers.py
@@ -1,22 +1,24 @@
 from rest_framework import serializers
 from .models import Paradigm,Language,Programmer
-
-class LanguageSerializer(serializers.HyperlinkedModelSerializer):
-    
-    class Meta:
-        model = Language
-        fields = ('id','url','name','paradigm')
-        
-
 class ParadigmSerializer(serializers.HyperlinkedModelSerializer):
     
     class Meta:
         model = Paradigm
         fields = ('id','url','name')
+
+
+class LanguageSerializer(serializers.HyperlinkedModelSerializer):
+    paradigm = ParadigmSerializer(many=False)
+    class Meta:
+        model = Language
+        fields = ('id','url','name','paradigm')
+        
+
         
 class ProgrammerSerializer(serializers.HyperlinkedModelSerializer):
-    
+    languages = LanguageSerializer(many=True)
     class Meta:
         model = Programmer
         fields = ('id','url','name','languages')
+        
         
