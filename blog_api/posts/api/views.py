deba756fbee40631f2d2fd4f5bdd59d20f5e5301
mahidulmoon <mahidulmoon@gmail.com>
2020-09-17 12:37:51 +0600

    ep11 complete (customize user permission(basepermission))

--- blog_api/posts/api/views.py
+++ blog_api/posts/api/views.py
@@ -1,6 +1,8 @@
 from rest_framework.generics import CreateAPIView,ListAPIView, RetrieveAPIView,UpdateAPIView,DestroyAPIView
 from ..models import Post
 from .serializers import PostSerializer,PostDetailSerializer,PostCreateSerializer
+from rest_framework.permissions import AllowAny,IsAuthenticated,IsAdminUser,IsAuthenticatedOrReadOnly
+from .permissions import IsOwnerOrReadOnly
 
 class PostListApiView(ListAPIView):
     queryset = Post.objects.all()
@@ -15,6 +17,9 @@ class PostDetailAPIView(RetrieveAPIView):
 class PostUpdateAPIView(UpdateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostDetailSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
+    def parform_update(self,serializer):
+        serializer.save(user=self.request.user)
 
 class PostDstroyAPIView(DestroyAPIView):
     queryset = Post.objects.all()
@@ -24,3 +29,7 @@ class PostDstroyAPIView(DestroyAPIView):
 class PostCreateAPIView(CreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostCreateSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def parform_create(self,serializer):
+        serializer.save(user=self.request.user)
