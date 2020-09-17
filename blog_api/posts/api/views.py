470d4c97b8036e2816a7e0757e6870918dfb6cfb
mahidulmoon <mahidulmoon@gmail.com>
2020-09-17 12:54:42 +0600

    ep12 complete (buildin filtering and Q filtering)

--- blog_api/posts/api/views.py
+++ blog_api/posts/api/views.py
@@ -3,10 +3,25 @@ from ..models import Post
 from .serializers import PostSerializer,PostDetailSerializer,PostCreateSerializer
 from rest_framework.permissions import AllowAny,IsAuthenticated,IsAdminUser,IsAuthenticatedOrReadOnly
 from .permissions import IsOwnerOrReadOnly
-
+from django.db.models import Q
+from rest_framework.filters import SearchFilter,OrderingFilter
 class PostListApiView(ListAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    filter_backends = [SearchFilter,OrderingFilter]
+    search_fields = ['id','title']
+    # def get_queryset(self,*args,**kwargs):
+    #     queryset_list = Post.objects.all()
+    #     query = self.request.GET.get['q']
+    #     if query:
+    #         queryset_list = queryset_list.filter(
+    #             Q(title_icontains=query) |
+    #             Q(content_icontains=query) |
+    #             Q(user__first_name__icontains=query) |
+    #             Q(title__last_name_icontains=query) 
+    #         ).distinct()
+    #     return queryset_list
+
 
 
 class PostDetailAPIView(RetrieveAPIView):
